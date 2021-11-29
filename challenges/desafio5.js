ca421f469211ccab0a9a0e0c39fb447899905682
Alexandre Altgauzen <82180840+altgauzen@users.noreply.github.com>
2021-11-29 19:52:49 +0000

    desafio 5 refatorado 1.1

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,25 +1,33 @@
+const favActTrybe = [
+  "Sandra Bullock",
+  "Tom Hanks",
+  "Julia Roberts",
+  "Kevin Spacey",
+  "George Clooney",
+];
 db.movies.aggregate([
+  {
+    $match: {
+      countries: "USA",
+      "tomatoes.viwer.rating": {
+        $gte: 3,
+      },
+    },
+  },
   {
     $project: {
       cast: 1,
       favMatch: {
         $setIntersection: [
           "$cast",
-          [
-            "Sandra Bullock",
-            "Tom Hanks",
-            "Julia Roberts",
-            "Kevin Spacey",
-            "George Clooney",
-          ],
+          favActTrybe,
         ],
       },
       _id: 0,
     },
   },
   {
-    $project: {
-      _id: 0,
+    $addFields: {
       num_favs: {
         $cond: {
           if: {
@@ -28,34 +36,28 @@ db.movies.aggregate([
           then: {
             $size: "$favMatch",
           },
-          else: "NA",
+          else: 0,
         },
       },
     },
-  },
-  {
-    $match: {
-      countries: "USA",
-      "tomatoes.viwer.rating": { $gte: 3 },
-    },
-  },
-  {
-    $project: {
-      _id: 0,
-      title: 1,
-    },
-  },
-  {
-    $skip: 24,
-  },
-  {
-    $limit: 1,
   },
   {
     $sort: {
       num_favs: -1,
       "tomatoes.viwer.rating": -1,
       title: -1,
+    },
+  },
+  {
+    $skip: 24,
+  },
+  {
+    $limit: 25,
+  },
+  {
+    $project: {
+      _id: 0,
+      title: 1,
     },
   },
 ]);
