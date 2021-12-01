7cdcbcfeeede0972314e87e7a5f9848b351fa59a
Alexandre Altgauzen <82180840+altgauzen@users.noreply.github.com>
2021-12-01 18:19:43 +0000

    desafio 10 finalizado

---
+++ challenges/desafio10.js
@@ -0,0 +1,27 @@
+db.trips.aggregate([
+  {
+    $project: {
+      _id: 0,
+      tipo: "$usertype",
+      duracao: {
+        $divide: [
+          { $subtract: ["$stopTime", "$startTime"] },
+          60 * 1000 * 60,
+        ],
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$tipo",
+      duracaoMed: { $avg: "$duracao" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: { $round: ["$duracaoMed", 2] },
+    },
+  },
+]);
